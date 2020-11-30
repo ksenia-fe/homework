3ceafa987d24b6721aa555a3c97702a3f96d1bbf
Ksenia Belikova <kseniabelikova35@gmail.com>
2020-11-30 12:59:56 +0200

    svg VS png

--- arr-delta.js
+++ arr-delta.js
@@ -1,16 +1,17 @@
 let clients = ['Ann', 'John', 'User'];
 let balances = [1400, 87, -6];
 let client = 'Ann';
-let amount = 1500;
+let amount = 574;
 
 function withdraw(clients, balances, client, amount){
   let indexOfPerson = clients.indexOf(client);
 
   if (balances[indexOfPerson] >= amount){
     balances[indexOfPerson] -= amount;
-    return balances;
+    return balances[indexOfPerson];
   }else if(balances[indexOfPerson] < amount){
     return -1;
+
   }
 
 }
